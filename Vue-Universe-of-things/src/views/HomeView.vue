c28d09ce48d97091b51d97a0a305ecd732ed4cba
Andrespz07 <andresperezortega2001@gmail.com>
2022-12-13 15:31:46 +0100

    HomeView

--- Vue-Universe-of-things/src/views/HomeView.vue
+++ Vue-Universe-of-things/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <img
+      src="src/assets/fondo-disney.PNG"
+      alt="Banner del centenario de Disney"
+    />
+    <div>
+    <h1>100 AÑOS DE EMOCIÓN</h1>
+<br>
+    <h2>Descubre todos nuestros personajes a lo largo de nuestra historia y elige tus favoritos</h2>
+<br>
+    <input type="button" value="VER PERONAJES">  
+  </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@import "../../public/sass/styles.scss";
+main{
+  background-color: $background-color; 
+    div {
+      justify-content: center;
+      
+    }
+}
+</style>
