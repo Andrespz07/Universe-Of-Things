66617b2e958ccbfea408d6d2b7e7ebc8062870df
Ramiro-isla <ramiroisla11@gmail.com>
2022-12-15 14:55:55 +0100

    routes done

--- Vue-Universe-of-things/src/router/index.js
+++ Vue-Universe-of-things/src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/CharactersView.vue"),
     },
+    {
+      path: "/favourites",
+      name: "favourites",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/FavouritesView.vue"),
+    },
   ],
 });
 export default router;
