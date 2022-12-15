c911a75d00935461d152fb9d8a1f7cc7b38ba9f1
Caren Salles <carencrisbsalles@gmail.com>
2022-12-15 12:38:47 +0100

    index changed

---
+++ Vue-Universe-of-things/src/components/CardCharacters.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="container">
+   <img src="" alt="">
+   
+   <h2></h2>
+   
+
+  </div>
+  
+<!--       
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. -->
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
