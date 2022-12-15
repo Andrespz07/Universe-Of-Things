ece0f17e31c729f7356f73bda1a7097046afa510
Caren Salles <carencrisbsalles@gmail.com>
2022-12-15 15:09:50 +0100

    header created

---
+++ Vue-Universe-of-things/src/components/Header.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <header>
+        <div class="container">
+            <img src="../assets/logo.svg">
+            <nav>
+                <ol>
+                    <li>Home</li>
+                    <li>Characters</li>
+                    <li>My Favourites</li>
+                </ol>
+            </nav>
+            
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header{
+    width: 100%;
+    background-color: white;
+    display: flex;
+
+}
+
+img{
+    width: ;
+}
+
+
+</style>
