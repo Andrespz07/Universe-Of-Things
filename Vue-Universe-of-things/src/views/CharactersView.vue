22efa3fbf579c6c2cf9516ff2b79d3238ffd64ce
Caren Salles <carencrisbsalles@gmail.com>
2022-12-20 11:14:26 +0100

    header and footer added

--- Vue-Universe-of-things/src/views/CharactersView.vue
+++ Vue-Universe-of-things/src/views/CharactersView.vue
@@ -8,6 +8,9 @@
 import { useCharactersStore } from "../stores/charactersStore";
 import { onBeforeMount } from "vue";
 import CardCharactersVue from "../components/CardCharacters.vue";
+import HeaderViewVue from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+import CardCharacters from "../components/CardCharacters.vue";
 
 const store = useCharactersStore();
 
@@ -30,5 +33,6 @@ onBeforeMount(async () => {
       />
     </div>
   </div>
+  <Footer> </Footer>
 </template>
 <style lang="scss" scoped></style>
